8c2972d02c7e18f540ba9f95fa2a6e9e26d1fd4b
Mohammad Yasin Memon <memonyasin3@gmail.com>
2020-03-16 15:20:28 +0530

    Removing of unic link of mongo db

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(userRoutes);
 
-mongoose.connect('mongodb+srv://yasin:Shariq2230@cluster0-kthvn.mongodb.net/client?retryWrites=true&w=majority').then(result => {
+mongoose.connect('').then(result => {
     app.listen(3000);
 }).catch(err => {
     console.log(err);
